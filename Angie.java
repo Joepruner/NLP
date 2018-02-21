7bc6dee9f2a2972b0f780e05ed901742bfe26f41
Kevin <kfeddema25@gmail.com>
2018-02-20 18:38:31 -0800

    Prepare repository for Sprint 1

--- Angie.java
+++
@@ -1,6 +0,0 @@
-public class AngieHello {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
-
--- eric.java
+++
@@ -1,7 +0,0 @@
-public class eric{
-public static void main(String[] args)
-{
- System.out.println("Hello World from Eric");
-}
-
-}
