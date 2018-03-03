205bd2413f6f2780cbdba7de0e74ed2170c4042b
Kevin <kfeddema25@gmail.com>
2018-03-03 13:34:54 -0800

    Add logo to body of index || Size form accordingly

--- sprint2/index.php
+++ sprint2/index.php
@@ -6,27 +6,27 @@
          <link rel="stylesheet" type="text/css" href="css/style.css">
      </head>
      <body>
-         <header>
+        <header>
              <a id="nl2cqHead" href="index.php">NL2CQ</a>
-         </header>
-         <body>
-             <form action="results.php" method="get">
-                 <fieldset>
-                    <label>Ask your question:</label>
-                    <input id="userInput" type="text" name="question">
-                    <input type="submit" class="largeButton" name="submit" value="SUBMIT">
-                 </fieldset>
-             </form>
-         </body>
-         <footer>
-             <span>The Phonetics</span>
-             <nav>
-                 <a href="about.html">About</a>
-                 <em> | </em>
-                 <a href="contact.html">Contact</a>
-                 <em> | </em>
-                 <a href="documentation.html">Documentation</a>
-             </nav>
-         </footer>
-     </body>
- </html>
+        </header>
+        <img class="mainLogo" src="res/NL2CQ%20Logo.svg">
+        <form action="results.php" method="get">
+            <fieldset>
+                <label>Ask your question:</label>
+                <input id="userInput" type="text" name="question">
+                <input type="submit" class="largeButton" name="submit" value="SUBMIT">
+            </fieldset>
+        </form>
+    
+        <footer>
+            <span>The Phonetics</span>
+            <nav>
+                <a href="about.html">About</a>
+                <em> | </em>
+                <a href="contact.html">Contact</a>
+                <em> | </em>
+                <a href="documentation.html">Documentation</a>
+            </nav>
+        </footer>
+    </body>
+</html>
