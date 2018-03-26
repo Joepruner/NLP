2b06644ff7fba646a5a43824ec2baa6327c717ff
apinchbeck <angie.pinchbeck@gmail.com>
2018-03-26 13:19:12 -0700

    code cleaned up

--- sprint3/Tokenize.py
+++ sprint3/Tokenize.py
@@ -5,7 +5,7 @@ This program tokenizes and assigns Stanford CoreNLP tags to a sentence. Input ca
 File name: Tokenize.py
 Author: Angie Pinchbeck, Joseph Pruner
 Date created: 27/02/2018
-Date last modified: 25/03/2018
+Date last modified: 26/03/2018
 Python version: 3.5
 
 Much of this was based on a tutorial from:
@@ -19,11 +19,10 @@ As much of possible, we have used the Google style guide for Python:
 """
 
 import nltk
+import sys
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from inflection import singularize
-#from pattern.text.en import singularize
 
 
 class Tokenize():
@@ -35,7 +34,7 @@ class Tokenize():
     Modifications:
         20/03/2018  Fixed __init__ to tag words before scrubbing them
         25/03/2018  Moved numberStartsWith() to be inside the Tokenize class
-        26/03/2018  Expanded the kept stop words to include "who".
+        26/03/2018  Expanded the kept stop words; added singularize method and removed PorterStemmer
 
     Attributes:
         keptStopWords(String[]): A list of words that we don't want to have scrubbed from input, even though
@@ -45,7 +44,6 @@ class Tokenize():
             Use the following two lines to see number of stop words and list of stop words:
             print (len(stopWords))  # Number of words
             print(stopWords)        # List of words
-        ps(PorterStemmer): An object that will allow the program to return only word stems.
         words (String[]): The words of the sentence that is being tokenized.
         wordsUnFiltered (String[]): The words and their Stanford CoreNLP tags attached, so far unfiltered of stop words.
         wordsTagged (Tuple(String, String)): The words and their Stanford CoreNLP tags attached, after being filtered
@@ -55,28 +53,14 @@ class Tokenize():
     
     keptStopWords = ["how", "all", "with", "have", "who", "and", "are", "is"]
     stopWords = set(stopwords.words('english')) - set(keptStopWords)
-    ps = PorterStemmer()
 
     def __init__(self, data):
         self.words = word_tokenize(data.lower())
         self.wordsUnFiltered = nltk.pos_tag(self.words)
         self.wordsTagged = []
         for wt in self.wordsUnFiltered:
-            if wt[0].lower() not in self.stopWords:
-                """
-                For stems of words instead of whole words, comment out:
-                self.wordsFiltered.append(wt)
-                Then comment in:
-                self.wordsFiltered.append(self.ps.stem(w))
-                """
-                self.wordsTagged.append(wt)
-                # self.wordsFiltered.append(self.ps.stem(w))
-
-    """ The next line can be used if we ever decide to deal in multiple sentences at one time. """
-    # self.wordsTagged.append(nltk.pos_tag(self.wordsFiltered))
-
             if wt[0] not in self.stopWords:
-                tuple = (singularize(wt[0].lower()), wt[1])
+                tuple = (singularize(wt[0]), wt[1])
                 self.wordsTagged.append(tuple)
 
     """
@@ -326,19 +310,12 @@ tagMap = t.wordsTagged
 #print(t.matchLabelAndProperty(tagMap))
 #print(t.numberStartsWith(tagMap))
 #print(t.listAllOf(tagMap))
-#print (t.returnName(tagMap))
+#print(t.returnName(tagMap))
 
 results = t.runTranslator(tagMap)
 for item in results:
     print(item)
 
-
-#print (string)
-#t = Tokenize(string)
-#tagMap = t.wordsTagged
-#print(t.wordsTagged)
-#print (t.wordsUnFiltered)
-#print (t.returnName(t.wordsUnFiltered))
 
 """
 while (data != 'e'):
