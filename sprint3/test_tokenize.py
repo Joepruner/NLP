5e79f05fadf8460dafe070c1c8f65bd211821063
apinchbeck <angie.pinchbeck@gmail.com>
2018-03-26 11:13:37 -0700

    fixed singularize and lowercase

--- sprint3/test_tokenize.py
+++ sprint3/test_tokenize.py
@@ -3,6 +3,8 @@ from Tokenize import Tokenize
 
 class TestTokenize(unittest.TestCase):
 
+    query1_1 = "MATCH (p :Person) RETURN p.name"
+    query1_2 = "MATCH (m :Movie) RETURN m.title"
     query5_1 = "MATCH (n) WHERE n names STARTS WITH \"J\" RETURN COUNT (n names)"
 
     """
@@ -28,6 +30,48 @@ class TestTokenize(unittest.TestCase):
         string = "How do you do"
         t = Tokenize(string)
         self.assertEqual(t.wordsTagged, [('How', 'WRB')]);
+
+    """
+    Test matchLabelAndProperty with query1_1, defined above.
+    """
+    def test1_matchLabelAndProperty(self):
+        string = "What are the names of all the people?"
+        t = Tokenize(string)
+        self.assertEqual(t.matchLabelAndProperty(t.wordsTagged), self.query1_1)
+
+    """
+    Test matchLabelAndProperty with query1_1, defined above, with lowercase.
+    """
+    def test2_matchLabelAndProperty(self):
+        string = "what are the names of all the people?"
+        t = Tokenize(string)
+        self.assertEqual(t.matchLabelAndProperty(t.wordsTagged), self.query1_1)
+
+    """
+    Test matchLabelAndProperty with query1_1, defined above, with uppercase.
+    """
+    def test3_matchLabelAndProperty(self):
+        string = "WHAT ARE THE NAMES OF ALL THE PEOPLE?"
+        t = Tokenize(string)
+        self.assertEqual(t.matchLabelAndProperty(t.wordsTagged), self.query1_1)
+
+    """
+    Test matchLabelAndProperty with a "how many" question; should return -1.
+    """
+
+    def test4_matchLabelAndProperty(self):
+        string = "How many names start with J?"
+        t = Tokenize(string)
+        self.assertEqual(t.matchLabelAndProperty(t.wordsTagged), -1)
+
+    """
+    Test matchLabelAndProperty with query1_2, defined above.
+    """
+
+    def test1_matchLabelAndProperty(self):
+        string = "What are all the movie titles?"
+        t = Tokenize(string)
+        self.assertEqual(t.matchLabelAndProperty(t.wordsTagged), self.query1_2)
 
     """
     Test for Kevin Feddema's numberStartsWith method for query5_1, defined above, with lowercase.
